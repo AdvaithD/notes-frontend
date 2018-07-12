58af9d9f8facd61efc8b45e4a7b7aef7678b618c
Advaith Doosa <advaith@chainist.com>
2018-07-11 19:20:39 -0700

    Initial commit. Basic login logout, routing, AWS amplify setup

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-gskzae55fo14"
+    },
+    apiGateway: {
+      REGION: "YOUR_API_GATEWAY_REGION",
+      URL: "https://fzmuyhruw9.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-west-2_w1FMyarhc",
+      APP_CLIENT_ID: "4vicmb131rek4esf2eutj0vda6",
+      IDENTITY_POOL_ID: "us-west-2:ede2f0d3-2cd1-4600-b8f3-552ff4201b4e"
+    }
+  };
+  
